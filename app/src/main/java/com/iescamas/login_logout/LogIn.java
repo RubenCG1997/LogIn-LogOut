2ec149266165653d1616c93b5db7fc8d25a22b36
RubenCG1997 <camachogomez1597@gmail.com>
2025-01-25 16:28:18 +0100

    Ejercicio completado

---
+++ app/src/main/java/com/iescamas/login_logout/LogIn.java
@@ -0,0 +1,87 @@
+package com.iescamas.login_logout;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LogIn extends AppCompatActivity {
+
+    private EditText txt_User,txt_Password;
+    private Button btn_Send;
+    private SharedPreferences.Editor editor;
+    private String msg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        initComponent();
+        leerPreferencias();
+
+        btn_Send.setOnClickListener(v -> {
+            if(txt_User.getText().toString().trim().isEmpty() ||
+            txt_Password.getText().toString().trim().isEmpty()){
+                Toast toast = Toast.makeText(
+                        getApplicationContext(),msg,Toast.LENGTH_SHORT
+                );
+                toast.show();
+            }
+            else{
+                editor.putString("User",txt_User.getText().toString());
+                editor.putString("Password",txt_Password.getText().toString());
+                editor.apply();
+                cambiarIntent();
+            }
+        });
+
+
+
+    }
+
+    private void initComponent(){
+
+        txt_User = findViewById(R.id.txt_User);
+        txt_Password = findViewById(R.id.txt_Password);
+        btn_Send = findViewById(R.id.btn_Send);
+        msg =  getResources().getString(R.string.msg_Toast);
+        txt_User.requestFocus();
+
+    }
+
+    private void leerPreferencias(){
+
+        String user,password;
+
+        SharedPreferences sharedPreferences;
+        sharedPreferences = getSharedPreferences("My Preferences",Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
+        user = sharedPreferences.getString("User","");
+        password = sharedPreferences.getString("Password","");
+
+
+        if (!user.isEmpty() && !password.isEmpty()){
+            cambiarIntent();
+        }
+    }
+
+    private void cambiarIntent(){
+        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
+}
---
+++ app/src/main/java/com/iescamas/login_logout/MainActivity.java
@@ -0,0 +1,81 @@
+package com.iescamas.login_logout;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.appbar.MaterialToolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MaterialToolbar materialToolbar;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+    private TextView lbl_Info;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        String user;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initComponent();
+        user = leerPreferencias();
+        lbl_Info.setText("Hola "+user+"\nBienvenido/a!");
+
+    }
+
+    private void initComponent(){
+
+        materialToolbar =findViewById(R.id.materialToolbar);
+        setSupportActionBar(materialToolbar);
+        lbl_Info = findViewById(R.id.lbl_Info);
+        sharedPreferences = getSharedPreferences("My Preferences", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
+    }
+
+    private String leerPreferencias(){
+        String user;
+        sharedPreferences = getSharedPreferences("My Preferences",Context.MODE_PRIVATE);
+        user = sharedPreferences.getString("User","");
+        return user;
+    }
+
+    private void borrarPreferencias(){
+        editor.clear();
+        editor.apply();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId()==R.id.output){
+            Intent intent = new Intent(getApplicationContext(),LogIn.class);
+            startActivity(intent);
+            borrarPreferencias();
+            finish();
+        }
+        return true;
+    }
+
+}
